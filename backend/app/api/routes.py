721a5eb31067679bc6227d11f121d6d396c07d6f
gshimatu <gauthiershimatu81@gmail.com>
2025-12-26 22:29:42 +0100

    Mise à jour de la route, maintenant le fichier est réellement stocké, on connait son chemin et il est prêt pour Whisper

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi import APIRouter, UploadFile, File
 from app.core.logger import logger
+from app.utils.file_handler import save_uploaded_file
 
 router = APIRouter(prefix="/api")
 
 @router.post("/upload")
 async def upload_meeting(file: UploadFile = File(...)):
-    logger.info(f"File received: {file.filename}")
-    return {"message": "File uploaded successfully"}
+    file_path = save_uploaded_file(file)
+    logger.info(f"File saved at {file_path}")
+    return {
+        "message": "File uploaded successfully",
+        "file_path": str(file_path)
+    }
